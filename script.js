be9fef8f080267dc09bedf43ae1d4178a3fa4756
mswendell <mscottwendell@gmail.com>
2025-03-02 21:11:33 -0700

    added js menu

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-document.write("basic code");
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hnav-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
